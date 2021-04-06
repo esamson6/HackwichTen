ea540479a6294bfb7f65ed8d95fd4e92a05f73b4
esamson6 <erin.samson34@yahoo.com>
2021-04-06 10:27:51 -1000

    Commit Added Segment 2

--- HackwichTen/ViewController.swift
+++ HackwichTen/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        myLabel.text = "Index One selected on the Segmented Control"
+    
     }
 
+    @IBAction func segmentedcontrolPressed(_ sender: Any) {
+        
+        switch segmentedControl.selectedSegmentIndex
+        {
+            case 0:
+            myLabel.text = "Index One selected on the Segmented Control"
+            
+            case 1:
+                myLabel.text = "Index Two selected I got this!"
+                
+        case 2:
+            myLabel.text = "Woohoo, this makes sense now"
+            
+            default:
+               break
+        }
+        
 
-}
+            }
+        }
+        
+    
+    
 
